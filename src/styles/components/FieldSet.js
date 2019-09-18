d366161653ecfd132ed9071e1a1631a0ebc7e751
Praveen Bisht <prvnbist@gmail.com>
2019-09-18 21:54:36 +0530

    Removed: Redundant Code

--- src/styles/components/FieldSet.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-const FieldSet = styled.fieldset`
-	padding: ${props => `${props.theme.basePt * 2.75}px`};
-	margin-bottom: ${props => `${props.theme.basePt * 3}px`};
-`
-
-export default FieldSet
--- src/styles/components/Form.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-const Form = styled.form`
-	@media (max-width: 568px) {
-		width: 100%;
-	}
-`
-
-export default Form
